52e36022dcfa161541efb02b360e2ccbefca2854
hasonduong <lasduong12@gmail.com>
2024-10-19 21:57:36 +0700

    p1

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Chào Mai, anh có điều muốn nói. </br> Làm người yêu anh nhé!",
+  descriptionHeader:
+    "Hãy trả lời một cách thật lòng nhé. Nếu em im lặng và rời đi thì tức là em đã ĐỒNG Ý.",
+  buttonYes: "Em đồng ý",
+  buttonNo: "Em yêu thằng khác",
+  titleModar: "Cảm ơn cậu.",
+  descriptionModar:
+    "Cảm ơn công chúa của anh đã cho anh một cơ hội để anh có thể ở bên cạnh, che chở và bảo vệ em. </br> Anh biết anh hơi đẹp trai, lắm tài nhưng anh cũng có một trái tim chân thành. Em chính là cả thế giới của anh. </br> Tối mai anh sẽ đến đón em. Sẽ có một bất ngờ dành cho em.</br> I love you, my princess. <3",
+};
